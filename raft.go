b43f373c922b632b47337d9985646a3f0ad918d2
ddl-killer <1754009643@QQ.com>
2023-06-09 11:34:02 +0800

    feat: rename log function

--- raft.go
+++ raft.go
@@ -40,7 +40,7 @@ func NewConsensusModule(id int, peerIds []int, server *Server, ready <-chan inte
 	return cm
 }
 
-func (cm *ConsensusModule) log(format string, args ...any) {
+func (cm *ConsensusModule) debug(format string, args ...any) {
 	format = fmt.Sprintf("[%d] ", cm.id) + format
 	log.Printf(format, args...)
 }
@@ -48,21 +48,21 @@ func (cm *ConsensusModule) log(format string, args ...any) {
 func (cm *ConsensusModule) Report() (id int, term int, isLeader bool) {
 	cm.mu.Lock()
 	defer cm.mu.Unlock()
-	return cm.id, cm.currentTerm, cm.state == Leader
+	return cm.id, cm.currentTerm, cm.isLeader()
 }
 
 func (cm *ConsensusModule) RequestVote(args RequestVoteArgs, reply *RequestVoteReply) error {
 	cm.mu.Lock()
 	defer cm.mu.Unlock()
 
-	if cm.state == Dead {
+	if cm.isDead() {
 		return nil
 	}
 
-	cm.log("RequestVote: %+v [currentTerm=%d, votedFor=%d]", args, cm.currentTerm, cm.votedFor)
+	cm.debug("RequestVote: %+v [currentTerm=%d, votedFor=%d]", args, cm.currentTerm, cm.votedFor)
 
 	if args.Term > cm.currentTerm {
-		cm.log("... term out of date in RequestVote")
+		cm.debug("... term out of date in RequestVote")
 		cm.becomeFollower(args.Term)
 	}
 
@@ -74,7 +74,7 @@ func (cm *ConsensusModule) RequestVote(args RequestVoteArgs, reply *RequestVoteR
 		reply.VoteGranted = false
 	}
 	reply.Term = cm.currentTerm
-	cm.log("... RequestVote reply: %+v", reply)
+	cm.debug("... RequestVote reply: %+v", reply)
 	return nil
 }
 
@@ -82,26 +82,26 @@ func (cm *ConsensusModule) AppendEntries(args AppendEntriesArgs, reply *AppendEn
 	cm.mu.Lock()
 	defer cm.mu.Unlock()
 
-	if cm.state == Dead {
+	if cm.isDead() {
 		return nil
 	}
-	cm.log("AppendEntries: %+v [currentTerm=%d]", args, cm.currentTerm)
+	cm.debug("AppendEntries: %+v [currentTerm=%d]", args, cm.currentTerm)
 
 	if args.Term > cm.currentTerm {
-		cm.log("... term out of date in AppendEntries")
+		cm.debug("... term out of date in AppendEntries")
 		cm.becomeFollower(args.Term)
 	}
 
 	reply.Success = false
 	if args.Term == cm.currentTerm {
-		if cm.state != Follower {
+		if !cm.isFollower() {
 			cm.becomeFollower(args.Term)
 		}
 		cm.electionResetTime = time.Now()
 		reply.Success = true
 	}
 	reply.Term = cm.currentTerm
-	cm.log("... AppendEntries reply: %+v", reply)
+	cm.debug("... AppendEntries reply: %+v", reply)
 	return nil
 }
 
@@ -129,7 +129,7 @@ func (cm *ConsensusModule) Stop() {
 	cm.mu.Lock()
 	defer cm.mu.Unlock()
 	cm.state = Dead
-	cm.log("becomes Dead")
+	cm.debug("becomes Dead")
 }
 
 func (cm *ConsensusModule) runElectionTimer() {
@@ -137,7 +137,7 @@ func (cm *ConsensusModule) runElectionTimer() {
 	cm.mu.Lock()
 	termStarted := cm.currentTerm
 	cm.mu.Unlock()
-	cm.log("election timer started (%v), term (%d)", electionTimeout, termStarted)
+	cm.debug("election timer started (%v), term (%d)", electionTimeout, termStarted)
 
 	ticker := time.NewTicker(10 * time.Millisecond)
 	defer ticker.Stop()
@@ -146,13 +146,13 @@ func (cm *ConsensusModule) runElectionTimer() {
 
 		cm.mu.Lock()
 		if !cm.isFollower() && !cm.isCandidate() {
-			cm.log("in election timer state=%s, bailing out", cm.state)
+			cm.debug("in election timer state=%s, bailing out", cm.state)
 			cm.mu.Unlock()
 			return
 		}
 
 		if cm.currentTerm != termStarted {
-			cm.log("in election timer term changed from %d to %d, bailing out", termStarted, cm.currentTerm)
+			cm.debug("in election timer term changed from %d to %d, bailing out", termStarted, cm.currentTerm)
 			cm.mu.Unlock()
 			return
 		}
@@ -172,7 +172,7 @@ func (cm *ConsensusModule) startElection() {
 	preCurrentTerm := cm.currentTerm
 	cm.votedFor = cm.id
 	cm.electionResetTime = time.Now()
-	cm.log("becomes Candidate (currentTerm=%d)", cm.currentTerm)
+	cm.debug("becomes Candidate (currentTerm=%d)", cm.currentTerm)
 
 	votesReceived := 1
 
@@ -183,25 +183,25 @@ func (cm *ConsensusModule) startElection() {
 	for _, peerId := range cm.peerIds {
 		go func(peerId int) {
 			var reply RequestVoteReply
-			cm.log("sending RequestVote to %d: %+v", peerId, args)
+			cm.debug("sending RequestVote to %d: %+v", peerId, args)
 			err := cm.server.Call(peerId, "ConsensusModule.RequestVote", args, &reply)
 			if err == nil {
 				cm.mu.Lock()
 				defer cm.mu.Unlock()
-				cm.log("received RequestVoteReply %+v", reply)
+				cm.debug("received RequestVoteReply %+v", reply)
 				if !cm.isCandidate() {
-					cm.log("while waiting for reply, state = %s", cm.state)
+					cm.debug("while waiting for reply, state = %s", cm.state)
 					return
 				}
 				if reply.Term > preCurrentTerm {
-					cm.log("term out of date in RequestVoteReply")
+					cm.debug("term out of date in RequestVoteReply")
 					cm.becomeFollower(reply.Term)
 					return
 				}
 				if reply.Term == preCurrentTerm && reply.VoteGranted {
 					votesReceived += 1
 					if votesReceived*2 > len(cm.peerIds)+1 {
-						cm.log("wins election with %d votes", votesReceived)
+						cm.debug("wins election with %d votes", votesReceived)
 						cm.startLeader()
 						return
 					}
@@ -214,7 +214,7 @@ func (cm *ConsensusModule) startElection() {
 }
 
 func (cm *ConsensusModule) becomeFollower(term int) {
-	cm.log("becomes Follower term=(%d)", term)
+	cm.debug("becomes Follower term=(%d)", term)
 	cm.state = Follower
 	cm.currentTerm = term
 	cm.votedFor = -1
@@ -224,7 +224,7 @@ func (cm *ConsensusModule) becomeFollower(term int) {
 }
 
 func (cm *ConsensusModule) startLeader() {
-	cm.log("becomes Leader (currentTerm=%d)", cm.currentTerm)
+	cm.debug("becomes Leader (currentTerm=%d)", cm.currentTerm)
 	cm.state = Leader
 
 	go func() {
@@ -261,18 +261,18 @@ func (cm *ConsensusModule) leaderHeartbeats() {
 	for _, peerId := range cm.peerIds {
 		go func(peerId int) {
 			var reply AppendEntriesReply
-			cm.log("sending AppendEntries to %d: %+v", peerId, args)
+			cm.debug("sending AppendEntries to %d: %+v", peerId, args)
 			err := cm.server.Call(peerId, "ConsensusModule.AppendEntries", args, &reply)
 			if err == nil {
 				cm.mu.Lock()
 				defer cm.mu.Unlock()
-				cm.log("received AppendEntriesReply %+v", reply)
+				cm.debug("received AppendEntriesReply %+v", reply)
 				if !cm.isLeader() {
-					cm.log("while waiting for reply, state = %s", cm.state)
+					cm.debug("while waiting for reply, state = %s", cm.state)
 					return
 				}
 				if reply.Term > savedCurrentTerm {
-					cm.log("term out of date in AppendEntriesReply")
+					cm.debug("term out of date in AppendEntriesReply")
 					cm.becomeFollower(reply.Term)
 					return
 				}
