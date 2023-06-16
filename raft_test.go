770ffded2f5d66efe4b8fcba6c9830f605b4a5fc
ddl-killer <1754009643@QQ.com>
2023-06-16 12:34:36 +0800

    feat: test

--- raft_test.go
+++ raft_test.go
@@ -1,10 +1,87 @@
 package raft
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestElectionBasic(t *testing.T) {
-	h := NewHarness(t, 5)
+	h := NewHarness(t, 3)
 	defer h.Shutdown()
 
 	h.CheckSingleLeader()
+}
+
+func TestElectionLeaderDisconnect(t *testing.T) {
+	h := NewHarness(t, 3)
+	defer h.Shutdown()
+
+	origLeaderId, origTerm := h.CheckSingleLeader()
+
+	h.DisconnectPeer(origLeaderId)
+	sleepMs(350)
+
+	newLeaderId, newTerm := h.CheckSingleLeader()
+	if newLeaderId == origLeaderId {
+		t.Errorf("want new leader to be different from orig leader")
+	}
+	if newTerm <= origTerm {
+		t.Errorf("want newTerm <= origTerm, got %d and %d", newTerm, origTerm)
+	}
+}
+
+func TestElectionLeaderAndAnotherDisconnect(t *testing.T) {
+	h := NewHarness(t, 3)
+	defer h.Shutdown()
+
+	origLeaderId, _ := h.CheckSingleLeader()
+
+	h.DisconnectPeer(origLeaderId)
+	otherId := (origLeaderId + 1) % 3
+	h.DisconnectPeer(otherId)
+
+	sleepMs(450)
+	h.CheckNoLeader()
+
+	h.ReconnectPeer(otherId)
+	h.CheckSingleLeader()
+}
+
+func TestDisconnectAllThenRestore(t *testing.T) {
+	h := NewHarness(t, 3)
+	defer h.Shutdown()
+
+	sleepMs(100)
+	for i := 0; i < 3; i++ {
+		h.DisconnectPeer(i)
+	}
+	sleepMs(450)
+	h.CheckNoLeader()
+
+	for i := 0; i < 3; i++ {
+		h.ReconnectPeer(i)
+	}
+	h.CheckSingleLeader()
+}
+
+func TestElectionLeaderDisconnectThenReconnect(t *testing.T) {
+	h := NewHarness(t, 3)
+	defer h.Shutdown()
+	origLeaderId, _ := h.CheckSingleLeader()
+
+	h.DisconnectPeer(origLeaderId)
+
+	sleepMs(350)
+	newLeaderId, newTerm := h.CheckSingleLeader()
+
+	h.ReconnectPeer(origLeaderId)
+	sleepMs(150)
+
+	againLeaderId, againTerm := h.CheckSingleLeader()
+
+	if newLeaderId != againLeaderId {
+		t.Errorf("again leader id got %d; want %d", againLeaderId, newLeaderId)
+	}
+	if againTerm != newTerm {
+		t.Errorf("again term got %d; want %d", againTerm, newTerm)
+	}
 }
--- testharness.go
+++ testharness.go
@@ -2,16 +2,10 @@ package raft
 
 import (
 	"log"
-	"math/rand"
 	"sync"
 	"testing"
 	"time"
 )
-
-func init() {
-	log.SetFlags(log.Ltime | log.Lmicroseconds)
-	rand.Seed(time.Now().UnixNano())
-}
 
 type Harness struct {
 	mu sync.Mutex
@@ -34,6 +28,11 @@ type Harness struct {
 	// will pass to or from it).
 	connected []bool
 
+	// alive has a bool per server in cluster, specifying whether this server is
+	// currently alive (false means it has crashed and wasn't restarted yet).
+	// connected implies alive.
+	alive []bool
+
 	n int
 	t *testing.T
 }
@@ -43,10 +42,11 @@ type Harness struct {
 func NewHarness(t *testing.T, n int) *Harness {
 	ns := make([]*Server, n)
 	connected := make([]bool, n)
-	ready := make(chan interface{})
+	alive := make([]bool, n)
 	commitChans := make([]chan CommitEntry, n)
-	storages := make([]*MapStorage, n)
 	commits := make([][]CommitEntry, n)
+	ready := make(chan interface{})
+	storage := make([]*MapStorage, n)
 
 	// Create all Servers in this cluster, assign ids and peer ids.
 	for i := 0; i < n; i++ {
@@ -57,10 +57,11 @@ func NewHarness(t *testing.T, n int) *Harness {
 			}
 		}
 
-		storages[i] = NewMapStorage()
+		storage[i] = NewMapStorage()
 		commitChans[i] = make(chan CommitEntry)
-		ns[i] = NewServer(i, peerIds, storages[i], ready, commitChans[i])
+		ns[i] = NewServer(i, peerIds, storage[i], ready, commitChans[i])
 		ns[i].Serve()
+		alive[i] = true
 	}
 
 	// Connect all peers to each other.
@@ -76,14 +77,14 @@ func NewHarness(t *testing.T, n int) *Harness {
 
 	h := &Harness{
 		cluster:     ns,
-		storage:     storages,
+		storage:     storage,
 		commitChans: commitChans,
 		commits:     commits,
 		connected:   connected,
+		alive:       alive,
 		n:           n,
 		t:           t,
 	}
-
 	for i := 0; i < n; i++ {
 		go h.collectCommits(i)
 	}
@@ -96,12 +97,77 @@ func (h *Harness) Shutdown() {
 		h.connected[i] = false
 	}
 	for i := 0; i < h.n; i++ {
-		h.cluster[i].Shutdown()
+		if h.alive[i] {
+			h.alive[i] = false
+			h.cluster[i].Shutdown()
+		}
+	}
+	for i := 0; i < h.n; i++ {
+		close(h.commitChans[i])
 	}
 }
 
+func (h *Harness) DisconnectPeer(id int) {
+	tlog("Disconnect %d", id)
+	h.cluster[id].DisconnectAll()
+	for j := 0; j < h.n; j++ {
+		if j != id {
+			h.cluster[j].DisconnectPeer(id)
+		}
+	}
+	h.connected[id] = false
+}
+
+func (h *Harness) ReconnectPeer(id int) {
+	tlog("Reconnect %d", id)
+	for j := 0; j < h.n; j++ {
+		if j != id && h.alive[j] {
+			if err := h.cluster[id].ConnectToPeer(j, h.cluster[j].GetListenAddr()); err != nil {
+				h.t.Fatal(err)
+			}
+			if err := h.cluster[j].ConnectToPeer(id, h.cluster[id].GetListenAddr()); err != nil {
+				h.t.Fatal(err)
+			}
+		}
+	}
+	h.connected[id] = true
+}
+
+func (h *Harness) CrashPeer(id int) {
+	tlog("Crash %d", id)
+	h.DisconnectPeer(id)
+	h.alive[id] = false
+	h.cluster[id].Shutdown()
+
+	h.mu.Lock()
+	h.commits[id] = h.commits[id][:0]
+	h.mu.Unlock()
+}
+
+func (h *Harness) RestartPeer(id int) {
+	if h.alive[id] {
+		log.Fatalf("id=%d is alive in RestartPeer", id)
+	}
+	tlog("Restart %d", id)
+
+	peerIds := make([]int, 0)
+	for p := 0; p < h.n; p++ {
+		if p != id {
+			peerIds = append(peerIds, p)
+		}
+	}
+
+	ready := make(chan interface{})
+	h.cluster[id] = NewServer(id, peerIds, h.storage[id], ready, h.commitChans[id])
+	h.cluster[id].Serve()
+	h.ReconnectPeer(id)
+	close(ready)
+	h.alive[id] = true
+	sleepMs(20)
+}
+
 func (h *Harness) CheckSingleLeader() (int, int) {
-	for r := 0; r < 5; r++ {
+	for r := 0; r < 8; r++ {
 		leaderId := -1
 		leaderTerm := -1
 		for i := 0; i < h.n; i++ {
@@ -127,6 +193,105 @@ func (h *Harness) CheckSingleLeader() (int, int) {
 	return -1, -1
 }
 
+func (h *Harness) CheckNoLeader() {
+	for i := 0; i < h.n; i++ {
+		if h.connected[i] {
+			_, _, isLeader := h.cluster[i].cm.Report()
+			if isLeader {
+				h.t.Fatalf("server %d leader; want none", i)
+			}
+		}
+	}
+}
+
+func (h *Harness) CheckCommitted(cmd int) (nc int, index int) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
+	commitsLen := -1
+	for i := 0; i < h.n; i++ {
+		if h.connected[i] {
+			if commitsLen >= 0 {
+				if len(h.commits[i]) != commitsLen {
+					h.t.Fatalf("commits[%d] = %d, commitsLen = %d", i, h.commits[i], commitsLen)
+				}
+			} else {
+				commitsLen = len(h.commits[i])
+			}
+		}
+	}
+
+	for c := 0; c < commitsLen; c++ {
+		cmdAtC := -1
+		for i := 0; i < h.n; i++ {
+			if h.connected[i] {
+				cmdOfN := h.commits[i][c].Command.(int)
+				if cmdAtC >= 0 {
+					if cmdOfN != cmdAtC {
+						h.t.Errorf("got %d, want %d at h.commits[%d][%d]", cmdOfN, cmdAtC, i, c)
+					}
+				} else {
+					cmdAtC = cmdOfN
+				}
+			}
+		}
+		if cmdAtC == cmd {
+			index := -1
+			nc := 0
+			for i := 0; i < h.n; i++ {
+				if h.connected[i] {
+					if index >= 0 && h.commits[i][c].Index != index {
+						h.t.Errorf("got Index=%d, want %d at h.commits[%d][%d]", h.commits[i][c].Index, index, i, c)
+					} else {
+						index = h.commits[i][c].Index
+					}
+					nc++
+				}
+			}
+			return nc, index
+		}
+	}
+
+	h.t.Errorf("cmd=%d not found in commits", cmd)
+	return -1, -1
+}
+
+func (h *Harness) CheckCommittedN(cmd int, n int) {
+	nc, _ := h.CheckCommitted(cmd)
+	if nc != n {
+		h.t.Errorf("CheckCommittedN got nc=%d, want %d", nc, n)
+	}
+}
+
+func (h *Harness) CheckNotCommitted(cmd int) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
+	for i := 0; i < h.n; i++ {
+		if h.connected[i] {
+			for c := 0; c < len(h.commits[i]); c++ {
+				gotCmd := h.commits[i][c].Command.(int)
+				if gotCmd == cmd {
+					h.t.Errorf("found %d at commits[%d][%d], expected none", cmd, i, c)
+				}
+			}
+		}
+	}
+}
+
+func (h *Harness) SubmitToServer(serverId int, cmd interface{}) bool {
+	return h.cluster[serverId].cm.Submit(cmd)
+}
+
+func tlog(format string, a ...interface{}) {
+	format = "[TEST] " + format
+	log.Printf(format, a...)
+}
+
+func sleepMs(n int) {
+	time.Sleep(time.Duration(n) * time.Millisecond)
+}
+
 func (h *Harness) collectCommits(i int) {
 	for c := range h.commitChans[i] {
 		h.mu.Lock()
@@ -134,9 +299,4 @@ func (h *Harness) collectCommits(i int) {
 		h.commits[i] = append(h.commits[i], c)
 		h.mu.Unlock()
 	}
-}
-
-func tlog(format string, a ...interface{}) {
-	format = "[TEST] " + format
-	log.Printf(format, a...)
 }
